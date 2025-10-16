bc3f314e23a8a63e80bf16bb84748c222a6c4a66
pgupta9204 <pranjal.nr.gupta@gmail.com>
2025-10-16 08:14:17 -0500

    updated right side starting position - use path.jerryio for waypointing

--- src/main.cpp
+++ src/main.cpp
@@ -69,11 +69,17 @@ void competition_initialize() {}
 void autonomous() 
 {
     // set position to x:0, y:0, heading:0
-    chassis.setPose(0, 0, 0);
+    // chassis.setPose(0, 0, 0);
     // turn to face heading 90 with a very long timeout
     // chassis.turnToHeading(90, 100000);
 
-    chassis.moveToPoint(0, 48, 10000); // drive to x:0, y:24 with a very long timeout
+    // chassis.moveToPoint(0, 48, 10000); // drive to x:0, y:24 with a very long timeout
+
+    chassis.setPose(-54.8375, -15.975, 90);
+
+    
+    
+
 }
 
 /**
